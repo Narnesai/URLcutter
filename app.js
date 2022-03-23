51283cb07998a24439bec466dd4360f1d85c754f
Narnesai <96784027+Narnesai@users.noreply.github.com>
2022-03-23 16:35:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -207,6 +207,6 @@ app.get('/delete/:shortUrl',function(req,res){
 })
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000.");
 });
